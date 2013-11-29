53b29de1865ce989eb1d165bc87fc632a6a70041
musik <cgicn@qq.com>
2013-11-30 04:19:39 +0800

    remove data static

--- data/static/advers.php
+++
@@ -1,9 +0,0 @@
-<?php
-//File name: advers.php
-//Creation time: 2013-11-29 00:49:58
-
-if (!defined('IN_HANFOX')) exit('Access Denied');
-
-$static_data = array(
-);
-?>
--- data/static/links.php
+++
@@ -1,9 +0,0 @@
-<?php
-//File name: links.php
-//Creation time: 2013-11-29 00:49:58
-
-if (!defined('IN_HANFOX')) exit('Access Denied');
-
-$static_data = array(
-);
-?>
